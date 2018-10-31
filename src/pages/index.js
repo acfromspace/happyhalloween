aee0c4dc4e9b31bacfe1809929ce50cb97d23448
AC De Leon <aadeleon@sdsu.edu>
2018-10-30 20:50:17 -0700

    [+] react-player, spooky ghosts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactPlayer from 'react-player'
 
-const IndexPage = () => <div>Hello there</div>
+import spooky from '../images/spooky.gif'
+import '../components/style.css'
 
-export default IndexPage
+class index extends Component {
+  render() {
+    return (
+      <div>
+        <div className="player-wrapper">
+          <ReactPlayer
+            className="react-player"
+            config={{
+              youtube: {
+                playerVars: {
+                  showinfo: 0,
+                  modestbranding: 1,
+                },
+              },
+            }}
+            url="https://www.youtube.com/watch?v=XQBwrsElVog"
+            width="100%"
+            height="100%"
+            loop={true}
+            playing={true}
+          />
+        </div>
+        <img className="gastly" src={spooky} alt="spooky" />
+        <img className="haunter" src={spooky} alt="spooky" />
+        <img className="gengar" src={spooky} alt="spooky" />
+      </div>
+    )
+  }
+}
+
+export default index
